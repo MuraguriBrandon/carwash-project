6a973491f1df71306c72201e59bec204375ae512
MuraguriBrandon <mwangibrandon2@gmail.com>
2024-04-16 21:23:15 +0300

    email functionality added

---
+++ main.py
@@ -0,0 +1,22 @@
+import smtplib
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+EMAIL='mwangibrandon2@gmail.com'
+with smtplib.SMTP('smtp.gmail.com') as connection: #opening connection to smtp.gmail.com
+    print('Encrypting Connection')
+    connection.starttls() #TLS Encryption Security to gmail
+    print('Logging in to:', EMAIL)
+    connection.login(user=EMAIL, password='iybs sdgk ekyz kldx') #Log in to Gmail using the App Key
+    print('Sending Email to:',EMAIL, 'from:', EMAIL)
+    connection.sendmail(
+        from_addr=EMAIL,
+        to_addrs=EMAIL,
+        msg='This is a Test email'        
+    ) #Send Email by passing from_address, recepient_address and message
+    print('Email sent. Closing Connection')
+    connection.close()
+
